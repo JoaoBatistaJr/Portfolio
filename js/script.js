d6b84aedddc9cc50425de3c0aafc31b33df2fbc7
joaobatistajr <joaob.dev@gmail.com>
2023-08-17 23:12:36 -0300

    style: foi adicionado os estilos basicos para a secção formação

--- js/script.js
+++ js/script.js
@@ -1,54 +1,27 @@
-if (window.SimpleSlide) {
-  // eslint-disable-next-line no-new, no-undef
-  new SimpleSlide({
-    slide: 'quote', // nome do atributo data-slide="principal"
-    // eslint-disable-next-line no-tabs, no-mixed-spaces-and-tabs
-    time: 5000, // tempo de transição dos slides
-  });
+/** Toggle hamburguer menu */
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
 
-  // eslint-disable-next-line no-new, no-undef
-  new SimpleSlide({
-    slide: 'anuncios',
-    // eslint-disable-next-line no-tabs, no-mixed-spaces-and-tabs
-    time: 10000,
-    nav: true,
-  });
+/** Class Active para o tema dark */
+burger.addEventListener('click', () => nav.classList.toggle('active'));
+function mudaTema() {
+  document.body.classList.toggle('dark');
 }
 
-if (window.SimpleAnime) {
-  // eslint-disable-next-line no-new, no-undef
-  new SimpleAnime();
-}
+/** Accordion - Formação */
+const accordionItems = document.querySelectorAll('.accordion-item');
 
-if (window.SimpleForm) {
-  // eslint-disable-next-line no-new, no-undef
-  new SimpleForm({
-    form: '.formphp', // seletor do formulário
-    button: '#enviar', // seletor do botão
-    erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
-    sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  const content = item.querySelector('.accordion-content');
+
+  header.addEventListener('click', () => {
+    content.classList.toggle('active');
+
+    if (content.classList.contains('active')) {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
   });
-}
-
-// Get the modal
-const modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-const img = document.getElementById('myImg');
-const modalImg = document.getElementById('img01');
-const captionText = document.getElementById('caption');
-// eslint-disable-next-line func-names
-img.onclick = function () {
-  modal.style.display = 'block';
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName('close')[0];
-
-// When the user clicks on <span> (x), close the modal
-// eslint-disable-next-line func-names
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+});
