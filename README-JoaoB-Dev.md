0c32ea4a1d2b9c5a1e410eb0c33095fab149585c
JoãoB <joaob.dev@gmail.com>
2025-03-03 16:28:47 -0300

    teste

---
+++ README-JoaoB-Dev.md
@@ -0,0 +1,71 @@
+
+<h1 align="center"> 📋 Portfólio </h1>
+<h3 align="center"> 🚀 Feito para mostrar meus projetos e habilidades como desenvolvedor.</h3>
+
+<p align="center">
+ <a href="#tecnologias">Tecnologias</a> • 
+ <a href="#licenc-a">Licença</a> • 
+ <a href="#autor">Autor</a>
+</p>
+
+##
+
+<h4 align="center"><a href="https://juniorsantos.vercel.app" target="_blank">   🔗 Clique aqui para acessar o Portfólio.</a></h4>
+
+##
+
+![image](https://github.com/JoaoBatistaJr/Portifolio/assets/21210172/352967e3-d1d9-4ce0-b7d7-d7d48bc1a3fe)
+![image](https://github.com/JoaoBatistaJr/Portifolio/assets/21210172/025e054b-513f-4c6f-a972-cb93ccdcb8d5)
+
+
+
+
+
+##
+
+<h3 id="tecnologias">🛠 Tecnologias</h3>
+<p>As seguintes ferramentas foram usadas na construção do projeto:</p>
+
+- [HTML5](https://www.w3schools.com/html/)
+- [CSS3](https://www.w3schools.com/css/)
+- [JavaScript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+- [SASS](https://sass-lang.com/documentation)
+- [Figma](https://www.figma.com)
+
+##
+
+<h3 id="licenc-a">⚖️ Licença</h3>
+
+![badge](https://img.shields.io/github/license/joaobatistajr/Portifolio?color=f&logo=MIT&logoColor=blue&style=for-the-badge)
+
+##
+
+<h3 id="autor">📝 Autor</h3>
+
+
+<a href="https://github.com/JoaoBatistaJr">
+ <img style="border-radius: 50%;" src="https://avatars3.githubusercontent.com/joaobatistajr" width="100px;" alt=""/>
+ <br />
+ <sub><b>João Batista Júnior</b></sub></a>
+ 
+#
+
+👋🏻 Meus contatos!
+
+<div aling="left">
+  <a href="https://www.instagram.com/joaob.dev/" alt="Instagram" target="_blank">
+    <img src="https://img.shields.io/badge/-Instagram-ff3a5e?style=for-the-badge&logo=Instagram&logoColor=white"/>
+  </a>
+  <a href = "mailto:joaob.dev@gmail.com" alt="Email" target="_blank">
+  <img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white">
+ </a>
+  <a href="https://www.linkedin.com/in/jbjunior03/" target="_blank">
+  <img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white">
+ </a> 
+</div>
+
+##
+	
+
+
+
---
+++ js/script-JoaoB-Dev.js
@@ -0,0 +1,110 @@
+/** Toggle hamburguer menu */
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+const menuItems = document.querySelectorAll(".nav-list");
+
+// Função para o menu Ativo
+
+
+// Função para fechar o menu mobile
+function closeMobileMenu() {
+  nav.classList.remove("active");
+}
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+// Adicione um ouvinte de evento de clique a cada item de menu
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+    closeMobileMenu(); // Fecha o menu mobile ao clicar em um item de menu
+  });
+});
+
+function mudaTema() {
+  const body = document.body;
+  const isDarkMode = !body.classList.contains("dark"); // Invertendo o estado atual
+
+  body.classList.toggle("dark", isDarkMode);
+
+  // Salva o estado do modo escuro no localStorage
+  localStorage.setItem("darkMode", isDarkMode);
+}
+
+// Verifique o estado do modo escuro no localStorage ao carregar a página
+document.addEventListener("DOMContentLoaded", function () {
+  const isDarkMode =
+    localStorage.getItem("darkMode") === "true" ||
+    !window.matchMedia("(prefers-color-scheme: light)").matches;
+
+  if (isDarkMode) {
+    document.body.classList.add("dark");
+  }
+});
+
+
+/** Accordion - Formação */
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach((item, index) => {
+  const header = item.querySelector(".accordion-header");
+  const content = item.querySelector(".accordion-content");
+  const tagsHeader = item.querySelector(".tags-formacao-header");
+  const icon = item.querySelector(".icon");
+
+  if (index === 0) {
+    // Deixa o primeiro item aberto por padrão
+    content.style.display = "block";
+    tagsHeader.style.display = "none";
+    icon.classList.add("active", "open");
+  } else {
+    content.style.display = "none";
+    tagsHeader.style.display = "block";
+    icon.classList.remove("active", "open");
+  }
+
+  header.addEventListener("click", () => {
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      tagsHeader.style.display = "block";
+      icon.classList.remove("open");
+      icon.classList.add("close");
+    } else {
+      // Fecha todos os itens antes de abrir o item atual
+      accordionItems.forEach((otherItem, otherIndex) => {
+        const otherContent = otherItem.querySelector(".accordion-content");
+        const otherTagsHeader = otherItem.querySelector(
+          ".tags-formacao-header"
+        );
+        const otherIcon = otherItem.querySelector(".icon");
+
+        if (otherContent !== content) {
+          otherContent.style.display = "none";
+          otherTagsHeader.style.display = "block";
+          otherIcon.classList.remove("open");
+          otherIcon.classList.add("close");
+        } else if (otherIndex === index) {
+          otherContent.style.display = "block";
+          otherTagsHeader.style.display = "none";
+          otherIcon.classList.remove("close");
+          otherIcon.classList.add("open");
+        }
+      });
+    }
+  });
+});
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+/** Função que atualiza sempre para o ano atual*/
+function atualizarAnoAtual() {
+  const data = new Date();
+  const ano = data.getFullYear();
+  document.getElementById("ano-atual").textContent = ano;
+}
+
+atualizarAnoAtual();
+
